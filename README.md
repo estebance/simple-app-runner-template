d4f4f145d5fa822bae8451c0b7b131b37040c380
Rafael Ceron Espinosa <rafaelceronespinosa@Rafaels-MacBook-Pro-2.local>
2023-05-19 14:59:52 -0500

    [FEATURE] init project

---
+++ README.md
@@ -0,0 +1,27 @@
+# backend-test-open-ai
+
+The project is based on ResearchGpt, to analyze documents content and talk about it with chatgpt. 
+We are using APP RUNNER to configure the infra
+
+## Deploy the sample application
+
+This time we are using python with Flask
+
+To use the SAM CLI, you need the following tools.
+
+* AWS APP RUNNER - [AWS APP RUNNER](https://aws.amazon.com/apprunner)
+* [Python 3 installed](https://www.python.org/downloads)
+* Docker - [Install Docker community edition](https://hub.docker.com/search/?type=edition&offering=community)
+
+## Run locally the application 
+
+Build the image 
+```
+	docker build -t open-ai .
+```
+TBC 
+
+### Additional resources 
+
+There is a CF file (infra.yaml) with additional infra resources
+
---
+++ app.py
@@ -0,0 +1,35 @@
+import json
+import os
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from wsgiref.simple_server import make_server
+
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/v1/echo', methods=['POST'])
+def check_comprehend():
+    if request.data is not None:
+        payload = request.json
+        data = payload['data']
+        document_id = data['document_id']
+        response = {'message': 'success', 'data': {'document_id': document_id}}
+        return jsonify(response), 200
+    else:
+        response = {'message': 'error'}
+        return jsonify(response), 500
+
+
+@app.route('/v1/healthcheck', methods=['GET'])
+def healthcheck():
+    return jsonify({'message': 'success'}), 200
+
+
+if __name__ == '__main__':
+    server = make_server('0.0.0.0', 3000, app)
+    server.serve_forever()
